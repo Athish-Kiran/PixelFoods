0fd17c0c93c6aeb28dd8122cba3322c05cb145d9
Mohit-Narayan <mohitnarayanmohit@gmail.com>
2025-06-15 01:14:40 +0530

    fix: update database configuration and build script for PostgreSQL compatibility

--- tindimithra/settings.py
+++ tindimithra/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'tindimithra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://tindimithra:rqwhxnfeFkOxEIaZ8fszGt1IWhLyJACL@dpg-d16qdmidbo4c73coh2f0-a/tindimithra',
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+if 'DATABASE_URL' in os.environ:
+    # Production database (PostgreSQL)
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    # Development database (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
