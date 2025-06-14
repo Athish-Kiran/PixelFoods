1f4e1cf9ead40c28ff7926d58235fdb4781deaf9
Mohit-Narayan <mohitnarayanmohit@gmail.com>
2025-06-15 05:00:01 +0530

    fix: simplify deployment configuration

--- pixel_foods/wsgi.py
+++ pixel_foods/wsgi.py
@@ -8,20 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-# Add your project directory to the sys.path
-path = '/home/mohitnarayan/cloudkitchen'
-if path not in sys.path:
-    sys.path.append(path)
-
-# Set the Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'pixel_foods.settings'
-
-# Activate your virtual environment
-activate_this = '/home/mohitnarayan/.virtualenvs/cloudkitchen/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pixel_foods.settings')
+
 application = get_wsgi_application()
